aac7eec537cb05aecf1c0df9e9b8a813195f45f1
diyamesss <jamesryan.developer@gmail.com>
2024-10-29 15:57:26 +0800

    Add dbInitializer for azure db deployment

---
+++ SpeedShip.DataAccess/DbInitializer/DbInitializer.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpeedShip.DataAccess.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeedShip.DataAccess.DbInitializer
+{
+	public class DbInitializer : IDbInitializer
+	{
+		public DbSpeedShipContext _dbSpeedShipContext;
+
+        public DbInitializer(DbSpeedShipContext dbSpeedShipContext)
+        {
+			_dbSpeedShipContext = dbSpeedShipContext;
+
+		}
+        public void Initialize()
+		{
+			try
+			{
+				if (_dbSpeedShipContext.Database.GetPendingMigrations().Count() > 0)
+				{
+					_dbSpeedShipContext.Database.Migrate();
+				}
+			}
+			catch (Exception ex)
+			{
+
+			}
+		}
+	}
+}
