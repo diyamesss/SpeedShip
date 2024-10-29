074e443adbe4a4255ed68eb9a3d4a796c514d381
diyamesss <jamesryan.developer@gmail.com>
2024-10-29 15:21:05 +0800

    Created Add and Update Category Pages Added appsettings for production

--- SpeedShip/Controllers/CategoryController.cs
+++ SpeedShip/Controllers/CategoryController.cs
@@ -8,20 +8,20 @@ namespace SpeedShip.Controllers
 	{
 		private readonly DbSpeedShipContext _dbSpeedShipContext;
 
-        public CategoryController(DbSpeedShipContext dbSpeedShipContext)
-        {
-            _dbSpeedShipContext = dbSpeedShipContext;
-        }
-
-        public IActionResult Index()
+		public CategoryController(DbSpeedShipContext dbSpeedShipContext)
 		{
-            return View(_dbSpeedShipContext.Categories.ToList());
+			_dbSpeedShipContext = dbSpeedShipContext;
 		}
 
-        public IActionResult Create()
-        {
-            return View();
-        }
+		public IActionResult Index()
+		{
+			return View(_dbSpeedShipContext.Categories.ToList());
+		}
+
+		public IActionResult Create()
+		{
+			return View();
+		}
 
 		[HttpPost]
 		public IActionResult Create(Category category)
@@ -34,9 +34,55 @@ namespace SpeedShip.Controllers
 			if (!ModelState.IsValid)
 				return View();
 
+			category.CreatedBy = "Admin";
+			category.DateCreated = DateTime.UtcNow;
 			_dbSpeedShipContext.Categories.Add(category);
 			_dbSpeedShipContext.SaveChanges();
-			return RedirectToAction("Index","Category");
+			return RedirectToAction("Index", "Category");
+		}
+
+		public IActionResult Update(long categoryId)
+		{
+			Category existingCategory = _dbSpeedShipContext.Categories.FirstOrDefault(c => c.CategoryId == categoryId);
+
+			return View(existingCategory);
+		}
+
+		[HttpPost]
+		public IActionResult Update(Category category)
+		{
+			Category existingCategory = _dbSpeedShipContext.Categories.FirstOrDefault(c => c.CategoryName == category.CategoryName && c.CategoryId != category.CategoryId);
+
+			if (existingCategory != null)
+				ModelState.AddModelError("CategoryName", "Category already exist");
+
+			if (!ModelState.IsValid)
+				return View(category);
+
+			category.UpdatedBy = "Admin";
+			category.DateUpdated = DateTime.UtcNow;
+			_dbSpeedShipContext.Categories.Update(category);
+			_dbSpeedShipContext.SaveChanges();
+			return RedirectToAction("Index", "Category");
+		}
+
+		public IActionResult Delete(long categoryId)
+		{
+			Category existingCategory = _dbSpeedShipContext.Categories.FirstOrDefault(c => c.CategoryId == categoryId);
+			return View(existingCategory);
+		}
+
+		[HttpPost, ActionName("Delete")]
+		public IActionResult DeleteCategory(long categoryId)
+		{
+			Category existingCategory = _dbSpeedShipContext.Categories.FirstOrDefault(c => c.CategoryId == categoryId);
+
+			if (existingCategory == null)
+				return RedirectToAction("Index", "Category");
+
+			_dbSpeedShipContext.Categories.Remove(existingCategory);
+			_dbSpeedShipContext.SaveChanges();
+			return RedirectToAction("Index", "Category");
 		}
 	}
 }
